2c82a16abb8b919ee2d6e39985134687cc27fdc9
smswork34-art <smswork34@gmail.com>
2026-02-06 04:00:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-// config.js - КОНФИГ ДЛЯ ФРОНТЕНДА (публичный ключ)
+// config.js
 window.SUPABASE_CONFIG = {
     url: 'https://okfakvtsevlyvbbfzyla.supabase.co',
-    // ИСПОЛЬЗУЙ ПУБЛИЧНЫЙ КЛЮЧ ЗДЕСЬ!
     key: 'sb_publishable_FY7dJEwFGZxImSE_Qyad9Q_M0zQGOY0',
-    botToken: '8546972046:AAFMR0WqJ0x_xBtosVmieypofIjHcnMtySY',
-    adminId: 8155919358
+    adminId: '8155919358'  // Твой Telegram ID
 };
