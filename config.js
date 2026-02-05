e2e5026b43c22bb8d3efe31f05d6fe5dae648731
smswork34-art <smswork34@gmail.com>
2026-02-06 03:48:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-const SUPABASE_URL = 'https://okfakvtsevlyvbbfzyla.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_FY7dJEwFGZxImSE_Qyad9Q_M0zQGOY0';
-const TELEGRAM_BOT_TOKEN = '8546972046:AAFMR0WqJ0x_xBtosVmieypofIjHcnMtySY'; // Замени!
-const ADMIN_CHAT_ID = 8155919358; // Твой ID!
+// config.js - КОНФИГ ДЛЯ ФРОНТЕНДА (публичный ключ)
+window.SUPABASE_CONFIG = {
+    url: 'https://okfakvtsevlyvbbfzyla.supabase.co',
+    // ИСПОЛЬЗУЙ ПУБЛИЧНЫЙ КЛЮЧ ЗДЕСЬ!
+    key: 'sb_publishable_FY7dJEwFGZxImSE_Qyad9Q_M0zQGOY0',
+    botToken: '8546972046:AAFMR0WqJ0x_xBtosVmieypofIjHcnMtySY',
+    adminId: 8155919358
+};
