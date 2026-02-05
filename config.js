a66d23444c7c9b0050a592f59023607c99e77a8e
smswork34-art <smswork34@gmail.com>
2026-02-06 04:16:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// config.js
+// config.js - Конфигурация проекта
 window.SUPABASE_CONFIG = {
     url: 'https://okfakvtsevlyvbbfzyla.supabase.co',
     key: 'sb_publishable_FY7dJEwFGZxImSE_Qyad9Q_M0zQGOY0',
-    adminId: '8155919358'  // Твой Telegram ID
+    adminId: '8155919358'
 };
