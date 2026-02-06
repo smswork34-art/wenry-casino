890118224b73e2d189732bf0d6d73b6a4eb9ab3f
smswork34-art <smswork34@gmail.com>
2026-02-06 06:15:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,10 @@ window.SUPABASE_CONFIG = {
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZmFrdnRzZXZseXZiYmZ6eWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMTcyNDYsImV4cCI6MjA4NTg5MzI0Nn0.xGB6HQjSv_VsU8jqFLIpU9m24NYim6HkuXwcRDZ4SqU',
     adminId: 8155919358
 };
+
+window.PAYMENT_CONFIG = {
+    provider: 'manual',
+    walletAddress: 'TYFgXbE1HMYp4Jeh1VoiYjqGoeZVLoxF9R',
+    minDeposit: 100,
+    maxDeposit: 100000
+};
